8aaf743b6af50dde892bf3961e7c97ac09e2e651
Benzo Benno <benzoclay@gmail.com>
2025-09-06 12:33:18 +0530

    Update validators.js

--- server/src/utils/validators.js
+++ server/src/utils/validators.js
@@ -1,1 +1,8 @@
+export function isValidEmail(email) {
+return /\S+@\S+\.\S+/.test(email);
+}
 
+
+export function isStrongPassword(pwd) {
+return typeof pwd === 'string' && pwd.length >= 8; // extend as needed
+}
